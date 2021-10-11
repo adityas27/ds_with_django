0d19ace60e392c91cd5d8d2272058fab8aee4f3d
Aditya Singh <adityasingh27072746@gmail.com>
2021-10-11 14:08:32 +0530

    Stoping this repo temproralily

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120, help_text="name of product")
+    image = models.ImageField(upload_to='products', default='no_picture.png')
+    price = models.FloatField(help_text="In Indian Rupees ₹")
+    created = models.DateTimeField(auto_now_add=True, help_text="Date when created")
+    updated = models.DateTimeField(auto_now=True, help_text="Date when updated")
+
+    def __str__(self):
+        return f"{self.name}-{self.created.strftime('%d/%m/%Y')}"
+    
